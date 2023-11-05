71b426ebbd44762ed539eff8c9eedf1e4c3a637f
yoshigawashimizu <yoshigawashimizi123@gmail.com>
2023-11-05 13:10:13 +0900

    在postcss里添加了说明笔记

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   plugins: {
     'postcss-px-to-viewport': {
+      // vw适配的标准屏的宽度 以iPhoneX作为默认标准宽度
+      // 设计图 750, 调成1倍 => 适配 375 标准屏幕
+      // 设计图 640, 调成1倍 => 适配 320 标准屏幕
       viewportWidth: 375
     }
   }
