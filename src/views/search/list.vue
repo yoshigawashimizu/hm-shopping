21ebbee3f43c08a79cb30abf6ec44ee38971c21f
yoshigawashimizu <yoshigawashimizi123@gmail.com>
2023-11-07 14:58:33 +0900

    创建搜索展示页静态结构

--- src/views/search/list.vue
+++ src/views/search/list.vue
@@ -1,14 +1,69 @@
 <template>
-  <div>
-    我是List
+  <div class="search">
+    <van-nav-bar fixed title="商品列表" left-arrow @click-left="$router.go(-1)" />
+
+    <van-search
+      readonly
+      shape="round"
+      background="#ffffff"
+      value="手机"
+      show-action
+      @click="$router.push('/search')"
+    >
+      <template #action>
+        <van-icon class="tool" name="apps-o" />
+      </template>
+    </van-search>
+
+    <!-- 排序选项按钮 -->
+    <div class="sort-btns">
+      <div class="sort-item">综合</div>
+      <div class="sort-item">销量</div>
+      <div class="sort-item">价格 </div>
+    </div>
+
+    <div class="goods-list">
+      <GoodsItem v-for="item in 10" :key="item"></GoodsItem>
+    </div>
   </div>
 </template>
 
 <script>
+import GoodsItem from '@/components/GoodsItem.vue' // 导入商品展示框组件
 export default {
-  name: 'ListIndex' // 搜索结果列表
+  name: 'ListIndex', // 搜索结果列表
+  components: {
+    GoodsItem
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.search {
+  padding-top: 46px;
+  ::v-deep .van-icon-arrow-left {
+    color: #333;
+  }
+  .tool {
+    font-size: 24px;
+    height: 40px;
+    line-height: 40px;
+  }
+
+  .sort-btns {
+    display: flex;
+    height: 36px;
+    line-height: 36px;
+    .sort-item {
+      text-align: center;
+      flex: 1;
+      font-size: 16px;
+    }
+  }
+}
+
+// 商品样式
+.goods-list {
+  background-color: #f6f6f6;
+}
 </style>
