d4550f2d4f2fd2c2e0dfec93d6bb48b56cbd2383
yoshigawashimizu <yoshigawashimizi123@gmail.com>
2023-11-07 12:17:34 +0900

    创建了商品展示框组件

---
+++ src/components/GoodsItem.vue
@@ -0,0 +1,77 @@
+<!-- 组件: "猜你喜欢","商品搜索结果"商品展示框 -->
+<template>
+  <!-- 判断: 商品的id goods_id 存在 -->
+  <div class="goods-item" @click="$router.push('/prodetail')" v-if="item.goods_id">
+    <div class="left">
+      <img :src="item.goods.image" alt="" />
+    </div>
+    <div class="right">
+      <p class="tit text-ellipsis-2">
+        三星手机 SAMSUNG Galaxy S23 8GB+256GB 超视觉夜拍系统 超清夜景 悠雾紫
+        5G手机 游戏拍照旗舰机s23
+      </p>
+      <p class="count">已售104件</p>
+      <p class="price">
+        <span class="new">¥3999.00</span>
+        <span class="old">¥6699.00</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GoodsItem', // 商品展示框组件
+  props: {
+    item: {
+      type: Object,
+      default: () => { // 如果传递的数据类型是对象, 则设置默认值时使用函数设置默认值, 不可以直接{}
+        return {}
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.goods-item {
+  height: 148px;
+  margin-bottom: 6px;
+  padding: 10px;
+  background-color: #fff;
+  display: flex;
+  .left {
+    width: 127px;
+    img {
+      display: block;
+      width: 100%;
+    }
+  }
+  .right {
+    flex: 1;
+    font-size: 14px;
+    line-height: 1.3;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+
+    .count {
+      color: #999;
+      font-size: 12px;
+    }
+    .price {
+      color: #999;
+      font-size: 16px;
+      .new {
+        color: #f03c3c;
+        margin-right: 10px;
+      }
+      .old {
+        text-decoration: line-through;
+        font-size: 12px;
+      }
+    }
+  }
+}
+</style>
--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,9 +1,14 @@
 // 按需引入,统一引入所需的的 vant-ui 组件
 import Vue from 'vue'
 
-import { Tabbar, TabbarItem, Icon, NavBar, Toast } from 'vant' // 这个步骤是不能少的
+import { Search, Swipe, SwipeItem, Grid, GridItem, Tabbar, TabbarItem, Icon, NavBar, Toast } from 'vant' // 这个步骤是不能少的
 Vue.use(Tabbar) // 导航组件
 Vue.use(TabbarItem) // 导航组件
 Vue.use(Icon) // 图标
 Vue.use(NavBar) // 导航栏
 Vue.use(Toast) // toast轻提示
+Vue.use(Search) // 搜索框
+Vue.use(Swipe) // 轮播图组件
+Vue.use(SwipeItem) // 轮播图组件
+Vue.use(Grid) // 布局组件
+Vue.use(GridItem) // 布局组件
