c963922da028cea8a78f83941c7ebedb7cc00414
yoshigawashimizu <yoshigawashimizi123@gmail.com>
2023-11-08 00:49:01 +0900

    将"从来源路由的请求路径中传递的分类页搜索关键字id 存入到state中"

--- src/store/module/searchList.js
+++ src/store/module/searchList.js
@@ -1,17 +1,19 @@
 // 这是搜索展示列表模块的状态管理的文件
 import { getProList } from '@/api/product.js' // 调用 axios 的二次封装类
+
 export default {
   namespaced: true, // 开启名称空间
   state () {
     return {
       querySearch: '', // 用户输入的搜索关键字
+      categoryId: '', // 分类页搜索关键字id
       page: 1, // 返回页码, 默认为1
       proList: [], // 返回搜索结果列表
       total: 0 // 返回数据总条数
     }
   },
   mutations: {
-    /** 发送请求获取到商品搜索结果列表
+    /** 发送自定义请求获取到商品搜索结果列表
      *
      * @param obj 查询传参
      * {
@@ -23,9 +25,10 @@ export default {
      * }
      * */
     async setProList (state, obj) {
-      state.querySearch = obj.goodsName// 将用户输入的搜索关键字存入 state 中
+      state.querySearch = obj.goodsName // 将用户输入的搜索关键字存入 state 中
+      state.categoryId = obj.categoryId // 将分类页搜索关键字id categoryId 存入 state 中
       const { data: { list } } = await getProList(obj)
-      console.log(list)
+      console.log('获取到的商品列表数组:', list) // 测试环境用, 搜索结构, 商品数据
       state.page = list.current_page
       state.proList = list.data
       state.total = list.total
