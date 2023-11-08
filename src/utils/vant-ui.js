41595e787eb9bd060fb2421b4b6c6d2e9fc4cf6f
yoshigawashimizu <yoshigawashimizi123@gmail.com>
2023-11-08 09:26:20 +0900

    完成了底部'加入购物车/立即购买'的弹层页面结构与动态渲染

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,7 +1,7 @@
 // 按需引入,统一引入所需的的 vant-ui 组件
 import Vue from 'vue'
 
-import { Search, Swipe, SwipeItem, Grid, GridItem, Tabbar, TabbarItem, Icon, NavBar, Toast, Lazyload, Rate } from 'vant' // 这个步骤是不能少的
+import { Search, Swipe, SwipeItem, Grid, GridItem, Tabbar, TabbarItem, Icon, NavBar, Toast, Lazyload, Rate, ActionSheet } from 'vant' // 这个步骤是不能少的
 Vue.use(Tabbar) // 导航组件
 Vue.use(TabbarItem) // 导航组件
 Vue.use(Icon) // 图标
@@ -13,4 +13,5 @@ Vue.use(SwipeItem) // 轮播图组件
 Vue.use(Grid) // 布局组件
 Vue.use(GridItem) // 布局组件
 Vue.use(Rate) // 评级组件
+Vue.use(ActionSheet) // 底部弹层组件
 Vue.use(Lazyload) // Lazyload是 Vue 指令，使用前需要对指令进行注册。
--- src/views/prodetail/index.vue
+++ src/views/prodetail/index.vue
@@ -76,9 +76,40 @@
         <van-icon name="shopping-cart-o" />
         <span>购物车</span>
       </div>
-      <div class="btn-add">加入购物车</div>
-      <div class="btn-buy">立刻购买</div>
+      <div class="btn-add" @click="addFn">加入购物车</div>
+      <div class="btn-buy" @click="buyFn">立刻购买</div>
     </div>
+
+    <!-- 弹层: 添加到购物车 -->
+    <van-action-sheet v-model="showPannel" :title="mode === 'cart'? '加入购物车' : '立即购买' ">
+      <div class="product">
+      <div class="product-title">
+        <div class="left">
+          <img :src="detail.goods_image" alt="">
+        </div>
+        <div class="right">
+          <div class="price">
+            <span>¥</span>
+            <span class="nowprice">{{ detail.goods_price_min }}</span>
+          </div>
+          <div class="count">
+            <span>库存</span>
+            <span>{{ detail.stock_total }}</span>
+          </div>
+        </div>
+      </div>
+      <div class="num-box">
+      <span>数量</span>
+      数字框占位
+      </div>
+      <!-- 有库存才显示购买按钮 -->
+      <div class="showbtn" v-if="detail.stock_total > 0">
+        <div class="btn" v-if="mode === 'cart'">加入购物车</div>
+        <div class="btn now" v-else>立刻购买</div>
+      </div>
+      <div class="btn-none" v-else>该商品已抢完</div>
+      </div>
+    </van-action-sheet>
   </div>
 </template>
 
@@ -94,7 +125,9 @@ export default {
       detail: {}, // 商品详细信息
       total: 0, // 商品评价总数
       commentList: [], // 评价列表
-      defaultImg // 无头像用户的默认头像地址
+      defaultImg, // 无头像用户的默认头像地址
+      showPannel: false, // 底部"加入购物车"弹层的伸缩开关状态
+      mode: 'cart' // 弹层状态, 通过状态管理弹层标题
     }
   },
   computed: {
@@ -104,6 +137,20 @@ export default {
     }
   },
   methods: {
+    /** 加入购物车按钮的点击事件
+     *
+     */
+    addFn () {
+      this.mode = 'cart' // 切换弹层状态为cart
+      this.showPannel = true // 开启弹层
+    },
+    /** 立即购买按钮的点击事件
+     *
+     */
+    buyFn () {
+      this.mode = 'buyNow' // 切换弹层状态为buy
+      this.showPannel = true // 开启弹层
+    },
     // 页面轮播图
     onChange (index) {
       this.current = index
@@ -280,5 +327,54 @@ export default {
 
 .tips {
   padding: 10px;
+}
+
+// 弹层样式
+.product {
+  .product-title {
+    display: flex;
+    .left {
+      img {
+        width: 90px;
+        height: 90px;
+      }
+      margin: 10px;
+    }
+    .right {
+      flex: 1;
+      padding: 10px;
+      .price {
+        font-size: 14px;
+        color: #fe560a;
+        .nowprice {
+          font-size: 24px;
+          margin: 0 5px;
+        }
+      }
+    }
+  }
+
+  .num-box {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+    align-items: center;
+  }
+
+  .btn, .btn-none {
+    height: 40px;
+    line-height: 40px;
+    margin: 20px;
+    border-radius: 20px;
+    text-align: center;
+    color: rgb(255, 255, 255);
+    background-color: rgb(255, 148, 2);
+  }
+  .btn.now {
+    background-color: #fe5630;
+  }
+  .btn-none {
+    background-color: #cccccc;
+  }
 }
 </style>
