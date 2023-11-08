d1d571e14e91815721647e2e4bf3a9018ad15681
yoshigawashimizu <yoshigawashimizi123@gmail.com>
2023-11-08 21:50:17 +0900

    正确的支付页样式

--- src/views/pay/index.vue
+++ src/views/pay/index.vue
@@ -248,3 +248,151 @@ export default {
     font-size: 14px;
   }
 }
+</style>
+.pay {
+  padding-top: 46px;
+  padding-bottom: 46px;
+  ::v-deep {
+    .van-nav-bar__arrow {
+      color: #333;
+    }
+  }
+}
+.address {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 20px;
+  font-size: 14px;
+  color: #666;
+  position: relative;
+  background: url(@/assets/border-line.png) bottom repeat-x;
+  background-size: 60px auto;
+  .left-icon {
+    margin-right: 20px;
+  }
+  .right-icon {
+    position: absolute;
+    right: 20px;
+    top: 50%;
+    transform: translateY(-7px);
+  }
+}
+.goods-item {
+  height: 100px;
+  margin-bottom: 6px;
+  padding: 10px;
+  background-color: #fff;
+  display: flex;
+  .left {
+    width: 100px;
+    img {
+      display: block;
+      width: 80px;
+      margin: 10px auto;
+    }
+  }
+  .right {
+    flex: 1;
+    font-size: 14px;
+    line-height: 1.3;
+    padding: 10px;
+    padding-right: 0px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    color: #333;
+    .info {
+      margin-top: 5px;
+      display: flex;
+      justify-content: space-between;
+      .price {
+        color: #fa2209;
+      }
+    }
+  }
+}
+
+.flow-num-box {
+  display: flex;
+  justify-content: flex-end;
+  padding: 10px 10px;
+  font-size: 14px;
+  border-bottom: 1px solid #efefef;
+  .money {
+    color: #fa2209;
+  }
+}
+
+.pay-cell {
+  font-size: 14px;
+  padding: 10px 12px;
+  color: #333;
+  display: flex;
+  justify-content: space-between;
+  .red {
+    color: #fa2209;
+  }
+}
+.pay-detail {
+  border-bottom: 1px solid #efefef;
+}
+
+.pay-way {
+  font-size: 14px;
+  padding: 10px 12px;
+  border-bottom: 1px solid #efefef;
+  color: #333;
+  .tit {
+    line-height: 30px;
+  }
+  .pay-cell {
+    padding: 10px 0;
+  }
+  .van-icon {
+    font-size: 20px;
+    margin-right: 5px;
+  }
+}
+
+.buytips {
+  display: block;
+  textarea {
+    display: block;
+    width: 100%;
+    border: none;
+    font-size: 14px;
+    padding: 12px;
+    height: 100px;
+  }
+}
+
+.footer-fixed {
+  position: fixed;
+  background-color: #fff;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 46px;
+  line-height: 46px;
+  border-top: 1px solid #efefef;
+  font-size: 14px;
+  display: flex;
+  .left {
+    flex: 1;
+    padding-left: 12px;
+    color: #666;
+    span {
+      color:#fa2209;
+    }
+  }
+  .tipsbtn {
+    width: 121px;
+    background: linear-gradient(90deg,#f9211c,#ff6335);
+    color: #fff;
+    text-align: center;
+    line-height: 46px;
+    display: block;
+    font-size: 14px;
+  }
+}
